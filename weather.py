c418c4228b4c557348c584bd9d1416983d01d450
Andrew Williams <Andrew.Williams@dec.wa.gov.au>
2007-08-02 11:43:32 +0000

    Mods for new cloud sensor

--- weather.py
+++ weather.py
@@ -6,17 +6,30 @@ DictCursor=safecursor.SafeCursor
 from globals import *
 
 #These are the initial defaults, copied to the status object on module init.
-_CloudOpenLevel=100   #Open if cloud < this for more than WeatherOpenDelay sec
-_CloudCloseLevel=150  #Close is cloud > this or raining
+_SkyOpenTemp=100   #Open if skytemp < this for more than WeatherOpenDelay sec
+_SkyCloseTemp=150  #Close is skytemp > this or raining
 _WeatherOpenDelay=1800  #Wait for 1800 sec of no-rain and cloud < CloudOpenLevel
 _CloudCloseDelay=150    #Wait until at least two cloud readings (1/2min) are 'cloudy'
 
 
+def _unyv(arg=0):
+  if arg == 0:
+    return "?"
+  elif arg == 1:
+    return "No"
+  elif arg == 2:
+    return "Yes"
+  elif arg == 3:
+    return "VERY!"
+  else:
+    return "error."
+
 def _yn(arg=0):
   if arg:
-    return 'yes'
+    return "Yes"
   else:
-    return 'no'
+    return "No"
+
 
 def render(v,dig,dp):
   """If v is None, return 'NULL' as a string. Otherwise return
@@ -33,59 +46,51 @@ class _Weather:
 
   def empty(self):
     "called by __init__ or manually to clear status"
-    self.lastmod=-1
-    self.cloud=0
-    self.sky=0
-    self.rain=0
-    self.temp = None
-    self.dewpoint = None
-    self.windspeed = None
-    self.windgust = None
-    self.winddir = None
-    self.pressure = None
-    self.weathererror=""
-    self.CloudCloseLevel=_CloudCloseLevel
-    self.CloudOpenLevel=_CloudOpenLevel
-    self.WeatherOpenDelay=_WeatherOpenDelay
-    self.CloudCloseDelay=_CloudCloseDelay    #No interface for setting this at runtime
-    self.OKforsec=86400   #Assume it's clear when we start up
-    self.CloudyForSec=0
-    self.clear=1
+    self.lastmod = -1
+    self.skytemp = 0.0
+    self.cloudf, self.windf, self.rainf, self.dayf = 0,0,0,0
+    self.rain = False
+    self.temp = 0.0
+    self.windspeed = 0.0
+    self.humidity = 0.0
+    self.dewpoint = 0.0
+    self.skylight = 0.0
+    self.rainhit,self.wethead = False,False
+    self.senstemp = 0.0
+    self.weathererror = ""
+    self.SkyCloseTemp = _SkyCloseTemp
+    self.SkyOpenLevel = _SkyOpenTemp
+    self.WeatherOpenDelay = _WeatherOpenDelay
+    self.CloudCloseDelay = _CloudCloseDelay    #No interface for setting this at runtime
+    self.OKforsec = 86400   #Assume it's clear when we start up
+    self.CloudyForSec = 0
+    self.clear = True
 
   def __init__(self):
     self.empty()
-    curs=db.cursor()
-    #Set the reference time for selecting a sky value to be 1800 seconds before now
-    #If we don't subtract 1800sec, the first query called will return no records.
-    try:
-      curs.execute('select cloud from weathlog where time > '+
-                    'from_unixtime(unix_timestamp(now())-1800) order by cloud limit 4' )
-      res=curs.fetchallDict()     
-      self.sky=res[-1]['cloud']   
-           #Take the 4th lowest cloud value in the last half hour
-    except:
-      self.weathererror="Error connecting to database for weather initialisation."
     self.update()
 
   def display(self):
     "Tells the status object to display itself"
-    print "Cloud level = ",self.cloud
-    print "Raining: ",_yn(self.rain)
-    print "Last weather entry: ",self.lastmod,"seconds ago."
-    print "Air temp:", render(self.temp,4,1)
-    print "Dew point:", render(self.dewpoint,4,1)
-    print "Avg wind speed:", render(self.windspeed,3,0)
-    print "Wind gust speed:", render(self.windgust,3,0)
-    print "Wind direction:", render(self.winddir,3,0)
-    print "Air Pressure:", render(self.pressure,6,1)
-    print "Current clear-sky value: ",self.sky
-    print "Becomes 'not clear' if cloud greater than:", 
-    print self.sky, "+", self.CloudCloseLevel, "=", self.sky+self.CloudCloseLevel
-    print "Becomes 'clear' if cloud less than",
-    print self.sky, "+", self.CloudOpenLevel, "=", self.sky+self.CloudOpenLevel,"for",
-    print self.WeatherOpenDelay,"seconds or more."
+    print "Sky Temp:  ", self.skytemp
+    print "Cloudy:    ", _unyv(self.cloudf)
+    print "Windy:     ", _unyv(self.windf)
+    print "Raining:   ", _unyv(self.rainf)
+    print "Daylight:  ", _unyv(self.dayf)
+    print "Last weather entry: ", self.lastmod,"seconds ago."
+    print "Air temp:  ", render(self.temp,4,1)
+    print "Avg wind:  ", render(self.windspeed,3,0)
+    print "Humidity:  ", render(self.humidity,2,0)
+    print "Dew point: ", render(self.dewpoint,4,1)
+    print "Raindrops: ", _yn(self.rainhit)
+    print "Wet sensor:", _yn(self.wethead)
+    print "Sens. Temp:", render(self.senstemp,4,1)
+    print
+    print "Becomes 'not clear' if skytemp warmer than:", self.SkyCloseTemp
+    print "Becomes 'clear' if skytemp colder than", self.SkyOpenTemp, "for",
+    print self.WeatherOpenDelay, "seconds or more."
     if self.weathererror:
-      print self.weathererror
+      print "Error: ", self.weathererror
     if self.clear:
       print "\nCurrent Status: Clear"
     else:
@@ -95,63 +100,58 @@ class _Weather:
 
   def checkweather(self):
     "Monitor Cloud and Rain data, and take action if necessary"
+    if self.rainf <> 1:  #0 is unknown, 1 is not raining, 2 is 'wet', 3 is raining.
+      self.rain = True
+    else:
+      self.rain = False
     if self.weathererror:
-      self.clear=0
-      self.OKforsec=0
+      self.clear = False
+      self.OKforsec = False
     else:
       if not self.clear:
-        if (self.cloud <= self.sky + self.CloudOpenLevel) and (not self.rain):
-          self.OKforsec=self.OKforsec+5
+        if (self.skytemp <= self.SkyOpenTemp) and (not self.rain):
+          self.OKforsec = self.OKforsec + 5
         else:
-          self.OKforsec=0
+          self.OKforsec = 0
         if self.OKforsec > self.WeatherOpenDelay:
-          self.clear=1
+          self.clear = True
       else:
-        if (self.cloud >= self.sky + self.CloudCloseLevel):
-          self.CloudyForSec=self.CloudyForSec+5
+        if (self.skytemp >= self.SkyCloseTemp):
+          self.CloudyForSec = self.CloudyForSec + 5
         else:
-          self.CloudyForSec=0
+          self.CloudyForSec = 0
         if (self.CloudyForSec > self.CloudCloseDelay) or self.rain:
-          self.clear=0
-          self.OKforsec=0
+          self.clear = False
+          self.OKforsec = 0
+
 
   def update(self, u_curs=None):
     "Connect to the database to update fields"
-    self.weathererror=""
+    self.weathererror = ""
 
     if not u_curs:
-      u_curs=db.cursor()
+      u_curs = db.cursor()
     try:
-      u_curs.execute('select unix_timestamp(now())-unix_timestamp(time) ' +
-          'as lastmod, cloud, rain, temp, dewpoint, windspeed, windgust, ' + 
-          'winddir, pressure from misc.weathlog order by time desc '+
+      u_curs.execute('select unix_timestamp(now())-unix_timestamp(time) as lastmod, ' +
+          'skytemp, cloudf, windf, rainf, dayf, temp, windspeed, humidity, dewpoint, ' +
+          'skylight, rainhit, wethead, senstemp from misc.weather order by time desc '+
           'limit 1')
                                        #Read the contents of the
                                        #'weather status' table to find
                                        #cloud voltage and rain status
       self.__dict__.update(u_curs.fetchallDict()[0])
     except:
-      self.weathererror="Weather database not OK, can't get current values"
-#    if self.lastmod>360:
-    if self.lastmod>540:
-      self.weathererror="Weather database not updated for " + `self.lastmod` + " seconds."
+      self.weathererror = "Weather database not OK, can't get current values"
+    if self.lastmod > 540:
+      self.weathererror = "Weather database not updated for " + `self.lastmod` + " seconds."
 
-    try:
-      u_curs.execute('select cloud from misc.weathlog where time > '+
-                    'from_unixtime(unix_timestamp(now())-1800) order by cloud limit 4' )
-      res=u_curs.fetchallDict()     
-      tempsky=res[-1]['cloud']   
-             #Take the 4th lowest cloud value in the last half hour
-      if tempsky < self.sky:
-        self.sky=tempsky      #Use this as the new sky value
-    except:
-      self.weathererror="Weather database not OK, can't get clear sky value"
     self.checkweather()
     self.updated()   #Call the 'updated' function to indicate fresh contents
 
 
   def updated(self):
     "Empty stub, override if desired. Called when status contents change"
+    pass
 
 
 
@@ -166,10 +166,10 @@ def _background():
 
 
 
-db=MySQLdb.Connection(host='cook', user='honcho', passwd='',
+db = MySQLdb.Connection(host='cook', user='honcho', passwd='',
                       db='misc', cursorclass=DictCursor)
-b_db=MySQLdb.Connection(host='cook', user='honcho', passwd='',
+b_db = MySQLdb.Connection(host='cook', user='honcho', passwd='',
                         db='misc', cursorclass=DictCursor)
-status=_Weather()
+status = _Weather()
 status.update()
 
