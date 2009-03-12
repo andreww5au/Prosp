c566436b0780bb155bab5cd9ad01ca2896b30aee
Observer <Andrew.Williams@dec.wa.gov.au>
2009-03-12 03:42:09 +0000

    Many changes by Ralph and Andrew during debugging

--- focuser/focus.c
+++ focuser/focus.c
@@ -1,7 +1,6 @@
 /*
 	PLAT software to control an FLI focuser
 	commands:
-
 		home - send the focuser to the home position
 		offset - relative offset from the current position
 		position - offset from the home position
@@ -22,21 +21,24 @@
 #include "libfli.h"
 #include "libfli-filter-focuser.h"
 
-#define BUFF_SIZ 4096
-        flidev_t dev[63];  /* FLI handles array */
+
+	flidev_t dev[63];  /* FLI handles array */
 
 int main(argc,argv)
 	int argc;
-	char *argv[];
-{
-	int focuserNumber;	
+	char **argv;
+{	int focuserNumber;	
 	char focuserCommand[64];
 	long numbersteps;
         char **list;
-	int error;
+	long error;
 	int i,kk,focus;
 	long focuserAt;
-	FLISetDebugLevel("localhost", FLIDEBUG_INFO);
+        char buff[2048];
+	size_t len;
+
+
+	FLISetDebugLevel("localhost", FLIDEBUG_WARN); 
 
 	if (argc<2){
 	        printf("	You must enter three parameters: \n");
@@ -44,15 +46,13 @@ int main(argc,argv)
 	        printf("		The focuser command (home,offset or position) and \n");
 	        printf("		The number of motor steps to move. \n");
 	     	return(EXIT_FAILURE);
-
-	}
-	if (argc == 4) {
+	} else if (argc == 4) {
 		focuserNumber = atoi(argv[1]);
 		strcpy(focuserCommand,argv[2]);
 		numbersteps = (long) atoi(argv[3]);
         	printf("	focuser number: %i \n", focuserNumber);
         	printf("	focuser command: %s \n",focuserCommand );
-        	printf("	number of motor steps to move: %i \n",(int) numbersteps); 
+        	printf("	number of motor steps to move: %i \n",(int) numbersteps);
 
 	} else {
 	        printf("	Too many or too few parameters. \n");
@@ -65,7 +65,7 @@ int main(argc,argv)
 
 	if ((error=FLIList(FLIDOMAIN_USB | FLIDEVICE_FOCUSER, &list)))
 	{
-	  fprintf(stderr,"ErrorFLIList:%s /n",strerror(error)); 
+	  fprintf(stderr,"ErrorFLIList:%s /n",strerror(error));
 	  return(EXIT_FAILURE);
 	}
 	kk=0;
@@ -79,8 +79,9 @@ int main(argc,argv)
                  }
 	     }
         }
+
 	if (kk == 0) {
-		printf ("	No FLI focus units attached. \n");
+		printf ("	No FLI focus units attached. %i \n",i);
 		return(EXIT_FAILURE);
 	} else if (focuserNumber >= kk){
 		printf ("	The highest FLI focuser number is %i. \n", kk-1);
@@ -88,20 +89,21 @@ int main(argc,argv)
 /*	} else	{
 		printf ("	%i FLI focus unit(s) attached. \n", kk); */
 	}
+
 /*      for each focuser on the list */
         for (i = 0; list[i] != NULL; i++) {
-           char buff[BUFF_SIZ];
 	   if((error=FLIOpen(&dev[i], list[i], FLIDOMAIN_USB | FLIDEVICE_FOCUSER))) {
-	      fprintf(stderr,"ErrorFLIOpen:%s \n",strerror(error)); 
+	      fprintf(stderr,"ErrorFLIOpen:%s \n",strerror(error));
 	      break;
 	   }
-	   if((error=FLIGetModel(dev[i],buff,BUFF_SIZ))) {
-	      fprintf(stderr,"Error FLIGetModel:%s \n",strerror(error)); 
+/*	   if((error=FLIGetModel(dev[i],buff, len ))) { */
+	   if((error=FLIGetModel(dev[i],buff, 2048 ))) {
+	      fprintf(stderr,"Error FLIGetModel:%s \n",strerror(error));
 	      break;
 	   }
            printf("	Model: %s \n", buff);
         }
-/*	printf("	Command: %s \n", focuserCommand); */
+	printf("	Command: %s \n", focuserCommand); 
 	if(strcmp(focuserCommand,"home")==0) {
 	   if((error=FLIHomeFocuser(dev[focuserNumber]))) {
 	      fprintf(stderr,"Error FLIHomeFocuser:%s \n",strerror(error)); 
@@ -125,6 +127,13 @@ int main(argc,argv)
 	     fprintf(stderr,"ErrorFLIFreeList:%s \n",strerror(error)); 
 	     return(EXIT_FAILURE);
 	}
+	if((error=FLIClose(dev[focuserNumber]))) {
+	     fprintf(stderr,"ErrorFLIClose:%s \n",strerror(error)); 
+	     return(EXIT_FAILURE);
+	}
+
+	printf("	FLI finished \n"); 
+
 	return(EXIT_SUCCESS);
 }
 
