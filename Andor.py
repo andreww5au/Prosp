fbaf3b878a77bf8cb57043d2d61131d973c9ed0b
Andrew Williams <andrew@physics.uwa.edu.au>
2012-10-01 22:56:34 +0800

    Add function to create a client object

--- Andor.py
+++ Andor.py
@@ -771,6 +771,12 @@ class Camera(object):
 
 
 
+def InitClient():
+  """Connect to the server process and create a proxy object to the
+     real camera object.
+  """
+  global camera
+  camera = Pyro4.Proxy('PYRONAME:AndorCamera')
 
 
 
